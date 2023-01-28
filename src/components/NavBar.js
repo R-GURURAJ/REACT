af7d427d600063b6e21bb20a4b36ea4a50b5172f
GURURAJ R <105771189+R-GURURAJ@users.noreply.github.com>
2023-01-28 09:31:51 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,12 +5,12 @@ function Navbar(){
         <div className="main_nav">
         <div className="nav">
                 <img src={youtube} alt="logo"width="80px"/>
-                <ol className="nav_list">
-                    <li>HOME</li>
-                    <li>SORTS</li>
-                    <li>SUBSCRIBE</li>
-                    <li>ABOUT US</li>
-                </ol>
+//                 <ol className="nav_list">
+//                     <li>HOME</li>
+//                     <li>SORTS</li>
+//                     <li>SUBSCRIBE</li>
+//                     <li>ABOUT US</li>
+//                 </ol>
         </div>
         </div>
     )
